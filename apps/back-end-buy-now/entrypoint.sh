c25935a20e46cac42d9c5f844a136575f513d07a
LailtonXavier <lailtonxavier123@gmail.com>
2025-12-25 20:10:39 -0300

    fix(backend): allow swagger

--- apps/back-end-buy-now/entrypoint.sh
+++ apps/back-end-buy-now/entrypoint.sh
@@ -2,15 +2,12 @@
 set -e
 
 echo "Aguardando Postgres..."
-
 until pg_isready -h "$POSTGRES_HOST" -p "$POSTGRES_PORT"; do
   sleep 1
 done
 
 echo "Postgres pronto! Aplicando migrations..."
-pnpm prisma:migrate
-
-echo "Iniciando aplicação..."
+pnpm prisma migrate deploy --schema=prisma/schema.prisma
 # pnpm start:dev
 
 node dist/main.js
