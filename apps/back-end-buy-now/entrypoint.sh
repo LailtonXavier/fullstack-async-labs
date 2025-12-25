b69c22b57e6da64c0b1ebac5f2b47ed4dc7e6adf
LailtonXavier <lailtonxavier123@gmail.com>
2025-12-25 20:26:47 -0300

    fix: cicd 2

--- apps/back-end-buy-now/entrypoint.sh
+++ apps/back-end-buy-now/entrypoint.sh
@@ -1,13 +1,14 @@
-#!/bin/sh -
+#!/bin/sh
 set -e
 
 echo "Aguardando Postgres..."
+
 until pg_isready -h "$POSTGRES_HOST" -p "$POSTGRES_PORT"; do
   sleep 1
 done
 
 echo "Postgres pronto! Aplicando migrations..."
-pnpm prisma migrate deploy --schema=prisma/schema.prisma
+npx prisma migrate deploy --schema=./prisma/schema.prisma
 # pnpm start:dev
 
 node dist/main.js
