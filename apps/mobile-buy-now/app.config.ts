96739b26dedec176b64273922e67c71fd8a7d3d0
LailtonXavier <lailtonxavier123@gmail.com>
2025-12-26 03:10:19 -0300

    fix: eas workspace configuration and hoisting

--- apps/mobile-buy-now/app.config.ts
+++ apps/mobile-buy-now/app.config.ts
@@ -4,21 +4,65 @@ export default {
     slug: 'mobile-buy-now',
     version: "1.1.0",
     runtimeVersion: "1.1.0",
+    orientation: "portrait",
+    icon: "./assets/images/icon.png",
     scheme: "buynow",
-    android: {
-      package: "com.lailtonnx.mobilebuynow"
-    },
+    userInterfaceStyle: "automatic",
+    platforms: ["android", "ios"],
+    newArchEnabled: true,
+    
     ios: {
+      supportsTablet: true,
       bundleIdentifier: "com.lailtonnx.mobilebuynow"
     },
+    
+    android: {
+      package: "com.lailtonnx.mobilebuynow",
+      adaptiveIcon: {
+        backgroundColor: "#E6F4FE",
+        foregroundImage: "./assets/images/android-icon-foreground.png",
+        backgroundImage: "./assets/images/android-icon-background.png",
+        monochromeImage: "./assets/images/android-icon-monochrome.png"
+      },
+      edgeToEdgeEnabled: true,
+      predictiveBackGestureEnabled: false
+    },
+    
+    web: {
+      output: "static",
+      favicon: "./assets/images/favicon.png"
+    },
+    
     updates: {
       url: "https://u.expo.dev/04337f45-9680-46c2-a5dc-21d47158d20b"
     },
+    
     extra: {
       eas: {
         projectId: "66693bb9-98c3-4693-9045-abf3f553ba3e"
       },
       EXPO_PUBLIC_OPENAI_API_KEY: process.env.EXPO_PUBLIC_OPENAI_API_KEY,
     },
+    
+    plugins: [
+      "expo-router",
+      [
+        "expo-splash-screen",
+        {
+          "image": "./assets/images/splash-icon.png",
+          "imageWidth": 200,
+          "resizeMode": "contain",
+          "backgroundColor": "#ffffff",
+          "dark": {
+            backgroundColor: "#000000"
+          }
+        }
+      ]
+    ],
+    
+    experiments: {
+      typedRoutes: true,
+      reactCompiler: true
+    }
   },
 };
