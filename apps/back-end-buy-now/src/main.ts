4a9e1dfc7cb3d7cbf06f759e528522fa93d1eed3
LailtonXavier <lailtonxavier123@gmail.com>
2025-12-26 00:26:25 -0300

    fix: back end cros

--- apps/back-end-buy-now/src/main.ts
+++ apps/back-end-buy-now/src/main.ts
@@ -1,32 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './infra/server/app.module';
 import { DomainExceptionFilter } from './shared/filters/domain-exception.filter';
-// import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-// import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    cors: {
-      origin: '*',
-    },
-  });
+  const app = await NestFactory.create(AppModule);
 
-  // const config = new DocumentBuilder()
-  //   .setTitle('API Documentation')
-  //   .setDescription('API Documentation')
-  //   .setVersion('1.0')
-  //   .addBearerAuth()
-  //   .build();
-
-  // const document = SwaggerModule.createDocument(app, config);
-
-  // SwaggerModule.setup('api-docs', app, document, {
-  //   swaggerOptions: {
-  //     persistAuthorization: true,
-  //   },
-  //   customCss: '.swagger-ui .topbar { display: none }',
-  //   customSiteTitle: 'API Documentation',
-  // });
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'https://fullstack-async-labs.vercel.app',
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    credentials: true,
+  }); 
 
   const port = process.env.PORT ?? 3000;
   app.useGlobalFilters(new DomainExceptionFilter());
